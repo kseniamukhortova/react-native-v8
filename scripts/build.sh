58f7cfdd017d4b9c2043d1f29c1e2ba5b641b10a
kseniamukhortova <k.mukhortova@roboforex.com>
2020-12-01 18:37:57 +0200

    v0.61.2-robo2

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,8 @@ source $(dirname $0)/env.sh
 ######################################################################################
 
 rm -rf $BUILD_DIR
-git clone --depth=1 --branch ${RN_VERSION} https://github.com/facebook/react-native.git $BUILD_DIR
+#git clone --depth=1 --branch ${RN_VERSION} https://github.com/facebook/react-native.git $BUILD_DIR
+git clone --depth=1 --branch v0.61.2-robo2 https://github.com/kseniamukhortova/react-native.git $BUILD_DIR
 
 PATCHSET=(
   # Patch React Native build to support v8runtime
